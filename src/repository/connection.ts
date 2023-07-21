c0bc8829dd61a004c829839a554c250a636c16f8
Jean Demel <demeljean@hotmail.fr>
2023-07-21 09:08:44 +0200

    connection cleanup

--- src/repository/connection.ts
+++ src/repository/connection.ts
@@ -2,3 +2,11 @@ import { MongoClient } from "mongodb";
 
 //Connexion à la base de données mongodb en se servant d'une variable d'environnement qui sera dans le .env
 export const connection = new MongoClient(process.env.DATABASE_URL!);
+
+const cleanup = () => {
+    connection.close();
+    process.exit();
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
